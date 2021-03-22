7d445051534e9fb7bdb3ecbb4a9d03c04e9baf81
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2021-03-22 14:44:39 -0300

    simple deck implementation

---
+++ generativemagic/decks.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+SUIT_CLUBS = 0
+SUIT_HEARTS = 1
+SUIT_SPADES = 2
+SUIT_DIAMONDS = 3
+
+
+def simple_deck():
+    """A deck Ace to King. This is NOT the united statess based new deck order!"""
+    return np.arange(1, 53)
+
+
+def random_deck():
+    """A random deck"""
+    return np.random.permutation(52) + 1
+
+
+def position_to_value(k):
+    assert 0 < k < 53
+    return (k - 1) % 13 + 1
+
+
+def position_to_suit(k):
+    assert 0 < k < 53
+    return int((k - 1) / 13)
+
+
+def position_to_suit_color(k):
+    assert 0 < k < 53
+    return int((k - 1) / 13) % 2
+
+
+def value_and_suit_to_card(value, suit):
+    assert 0 < value < 14 and 0 <= suit <= 3
+    return suit * 13 + value
+
+
+def value_and_suit_to_card_position_on_simple_deck(value, suit):
+    return value_and_suit_to_card(value, suit) - 1
+
+
+def is_int_card(card):
+    return isinstance(card, (int, np.integer))
---
+++ tests/generativemagic/test_decks.py
@@ -0,0 +1,35 @@
+from generativemagic.decks import SUIT_CLUBS, value_and_suit_to_card, position_to_suit, SUIT_DIAMONDS, SUIT_SPADES, \
+    SUIT_HEARTS, position_to_value
+
+
+def test_position_to_suit():
+    for i in range(13 * 0 + 1, 13 * 1 + 1):
+        assert position_to_suit(i) == SUIT_CLUBS
+    for i in range(13 * 1 + 1, 13 * 2 + 1):
+        assert position_to_suit(i) == SUIT_HEARTS
+    for i in range(13 * 2 + 1, 13 * 3 + 1):
+        assert position_to_suit(i) == SUIT_SPADES
+    for i in range(13 * 3 + 1, 13 * 4 + 1):
+        assert position_to_suit(i) == SUIT_DIAMONDS
+
+
+def test_position_to_value():
+    for i in range(1, 13 + 1):
+        assert position_to_value(SUIT_CLUBS * 13 + i) == i
+    for i in range(1, 13 + 1):
+        assert position_to_value(SUIT_HEARTS * 13 + i) == i
+    for i in range(1, 13 + 1):
+        assert position_to_value(SUIT_SPADES * 13 + i) == i
+    for i in range(1, 13 + 1):
+        assert position_to_value(SUIT_DIAMONDS * 13 + i) == i
+
+
+def test_value_and_suit_to_card():
+    for i in range(1, 13 + 1):
+        assert value_and_suit_to_card(i, SUIT_CLUBS) == SUIT_CLUBS * 13 + i
+    for i in range(1, 13 + 1):
+        assert value_and_suit_to_card(i, SUIT_HEARTS) == SUIT_HEARTS * 13 + i
+    for i in range(1, 13 + 1):
+        assert value_and_suit_to_card(i, SUIT_SPADES) == SUIT_SPADES * 13 + i
+    for i in range(1, 13 + 1):
+        assert value_and_suit_to_card(i, SUIT_DIAMONDS) == SUIT_DIAMONDS * 13 + i
