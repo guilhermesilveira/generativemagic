3e0532a3387685a4e7f56516866aac51c6c53889
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2021-04-15 20:14:13 -0300

    better logging

--- generativemagic/solver/solver.py
+++ generativemagic/solver/solver.py
@@ -1,3 +1,5 @@
+import logging
+
 from z3 import Solver
 
 from generativemagic.solver.rules import Ruler
@@ -9,9 +11,7 @@ class RuleSolver:
     def solve(self, ruler: Ruler):
 
         start_time = timeit.default_timer()
-        solver = Solver()
-        for rule in ruler.rules():
-            solver.add(rule)
+        solver = self._create_solver(ruler)
 
         print(f"Satisfiable? {solver.check()}")
 
@@ -20,12 +20,25 @@ class RuleSolver:
         print(solver.statistics())
 
         model = solver.model()
+        if model:
+            self._print_result(model)
+        else:
+            print("No model was found")
+
+    def _print_result(self, model):
         dic_model = {}
         for d in model.decls():
             dic_model[int(model[d].__str__())] = d.name()
-
         print("{")
         keys = sorted(list(dic_model.keys()))
         for k in keys:
             print(f"{k}: '{dic_model[k]}',")
         print("}")
+
+    def _create_solver(self, ruler):
+        solver = Solver()
+        unique_rules = set(ruler.rules())
+        logging.info(f"Using {len(unique_rules)} unique constraints")
+        for rule in unique_rules:
+            solver.add(rule)
+        return solver
